c53527d0238af430e8b608720e4d2f9af5851aac
Sumit5089 <sumitgaikwad930@gmail.com>
2024-06-27 01:05:06 +0530

    Homepage UI design

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -2,7 +2,46 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards <i class="fa fa-university" aria-hidden="true"></i></h1>
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-6 p-5">
+                    <img src="media\largest-broker.svg" alt="" />
+                </div>
+                <div className="col-6 p-5">
+                    <h1 className='mt-3'>Largest stock broker in India</h1>
+                    <p className='mb-5'>1.5+ Crore Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in</p>
+                    <div className="row">
+                        <div className="col-6"> 
+                        <ul>
+                            <li>
+                                <p>Futures and Options</p>
+                            </li>
+                            <li>
+                                <p>Commodity derivatives</p>
+                            </li>
+                            <li>
+                                <p>Currency derivatives</p>
+                            </li>
+                        </ul>
+                        </div>
+                        <div className="col-6">
+                        <ul>
+                            <li>
+                                <p>Stocks & IPOs</p>
+                            </li>
+                            <li>
+                                <p>Direct mutual funds</p>
+                            </li>
+                            <li>
+                                <p>Bonds and Govt. Securities</p>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    <img className='mx-3' src="media\press-logos.png" style={{width:"90%"}}/>
+                </div>
+            </div>
+        </div>
      );
 }
 
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <div className='container p-5'>
+        <div className='container p-5 mb-5'>
             <div className="row text-center">
-                <img className='mb-5' src="media/landing.png" alt="landing image" />
-                <h1 className='mt-5 fs-10'>Invest in everything</h1>
+                <img className='mb-5 ' src="media/landing.png" alt="landing image" style={{width:"60%", margin:"0 auto"}}/>
+                <h1 className='mt-2 fs-10'>Invest in everything</h1>
                 <p className='fs-5 mb-5'>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-                <button className='btn btn-primary p-2 fs-5' style={{width:"18%", margin:"0 auto"}}>Sign up Now</button>
+                <button className='btn btn-primary p-2 fs-5 mb-5' style={{width:"18%", margin:"0 auto"}}>Sign up Now</button>
             </div>
         </div>
      );
