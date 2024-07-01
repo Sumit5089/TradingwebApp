52264678c9eea0b527f4ab691f9c02dc582a602e
Sumit Gaikwad <132696606+Sumit5089@users.noreply.github.com>
2024-07-01 21:33:56 +0530

    Delete dashboard directory

--- dashboard/src/components/DoughnoutChart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-import { Doughnut } from "react-chartjs-2";
-
-ChartJS.register(ArcElement, Tooltip, Legend);
-
-export function DoughnutChart({ data }) {
-  return <Doughnut data={data} />;
-}
