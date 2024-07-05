8c109d0e450036d61c4cc8304f8408dfd841daa7
Sumit5089 <sumitgaikwad930@gmail.com>
2024-07-05 16:14:24 +0530

    express and mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
+// Initiate the server and listen on port 3000
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
-
+ const PORT = process.env.PORT || 3000;
+ const URL = process.env.MONGO_URL;
+ 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 3000');
+    mongoose.connect(URL);
+    console.log('Connected to MongoDB');
 });
